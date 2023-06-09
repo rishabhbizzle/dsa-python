426c1ba79e5ec7b0d86030a82f62deedf13ac440
rishabhbizzle <rishabhbizzle@gmail.com>
2023-06-09 21:05:53 +0530

    kadane algo

---
+++ Arrays/kadaneAlgo.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        summ = 0
+        maxi = nums[0]
+
+        for num in nums:
+            summ += num
+            if summ > maxi:
+                maxi = summ
+            if summ < 0:
+                summ = 0
+
+        return maxi
